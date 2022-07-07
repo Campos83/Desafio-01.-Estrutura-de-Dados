d18b787ad63d06d6bec436b8ee2af765810cef8b
Willian Campos <Campos83>
2022-07-07 14:11:05 -0300

    Desafio.1 Estrutura de Dados

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("1. Crie uma variável que receba um nome");
+const nomeCompleto = "Willian Campos"
+console.log(nomeCompleto);
+
+console.log("2. Crie uma variável que receba um número")
+let numero = 38
+console.log(numero);
+
+console.log("3. Crie uma variável com o ano atual e subtraia pelo número que você guardou");
+let num = 2022
+let menos = numero-num
+console.log(menos);
+
+console.log("Estou mexendo no console.log");
+
+const quartaFeira = false
+console.log(typeof quartaFeira);
+
